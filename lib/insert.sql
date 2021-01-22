eae50cb177d530fb9590a28a97cbbb5dc45d109d
oopanpan <oopanpan@gmail.com>
2021-01-22 00:54:10 -0800

    commit tree a566cf7141c01917ea0fda4abd1fb71196fadd8b

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears 
+(name, age, sex, color, temperament, alive) 
+VALUES 
+("Mr.Chocolate", 3, "M", "Brown", "gentle", true),
+("Rowdy", 5, "M", "Brown", "gentle", true), 
+("Tabitha", 19, "F", "Brown", "gentle", false), 
+("Sergeant Brown", 12, "M", "Brown", "gentle", false), 
+("Melissa", 3, "F", "Brown", "gentle", true), 
+("Grinch", 5, "M", "Brown", "nervous", true), 
+("Wendy", 10, "F", "Brown", "nervous", false), 
+(NULL, 3, "M", "Brown", "gentle", false);
